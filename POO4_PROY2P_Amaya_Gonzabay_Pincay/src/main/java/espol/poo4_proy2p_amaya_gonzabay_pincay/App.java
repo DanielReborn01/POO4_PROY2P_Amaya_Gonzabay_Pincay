583ec21bae2be5ffa166885116d231abbc8c1df3
Walter Gonzabay <wagonzab@espol.edu.ec>
2023-08-19 23:11:21 -0500

    Create vista Login y Modelo

--- POO4_PROY2P_Amaya_Gonzabay_Pincay/src/main/java/espol/poo4_proy2p_amaya_gonzabay_pincay/App.java
+++ POO4_PROY2P_Amaya_Gonzabay_Pincay/src/main/java/espol/poo4_proy2p_amaya_gonzabay_pincay/App.java
@@ -1,5 +1,6 @@
 package espol.poo4_proy2p_amaya_gonzabay_pincay;
 
+import java.io.FileInputStream;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import javafx.scene.image.Image;
 
 /**
  * JavaFX App
@@ -14,13 +16,37 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
-
+    public final String pathImage = "src/main/resources/Imagenes/" ;
+    
+    public static Format tam = Format.MEDIANO;
+    
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/login" + ".fxml"));
         Parent root = fxmlLoader.load();
         scene = new Scene(root, 750, 480);
+        
+        //Ponemos a la escucha para el Design Responsive
+        scene.widthProperty().addListener((o) -> {
+           
+           if(scene.getWidth() > 500 && scene.getWidth() < 1024 && !tam.equals(Format.MEDIANO)){
+               //Se coloca la vista del tamano mediano
+               System.out.println("Tamaño mediano");
+               tam = Format.MEDIANO;
+           }else if(scene.getWidth() < 500 && !tam.equals(Format.PEQUEÑO)){
+               System.out.println("Tamaño pequeño");
+            
+               tam = Format.PEQUEÑO;
+           }else if(scene.getWidth() > 1024 && !tam.equals(Format.FULL)){
+               System.out.println("Tamaño grande");
+               tam = Format.FULL;
+           }
+        });
+        
         stage.setScene(scene);
+        stage.setMinWidth(375);
+        stage.setMinHeight(450);
+        stage.getIcons().add(new Image(new FileInputStream(pathImage+"logo.png")));
         stage.show();
     }
 
---
+++ POO4_PROY2P_Amaya_Gonzabay_Pincay/src/main/java/espol/poo4_proy2p_amaya_gonzabay_pincay/Format.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package espol.poo4_proy2p_amaya_gonzabay_pincay;
+
+/**
+ *
+ * @author Walter G
+ */
+public enum Format {
+    FULL, MEDIANO, PEQUEÑO;
+}
